41aee21c08e189f551dd207246a1b65c7db9f5fd
nehad-elsayed <nehad.boody74@gmail.com>
2025-04-27 23:23:46 +0300

    handle modal for deleting note

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,4 +1,126 @@
 
+// import axios from "axios";
+// import { useFormik } from "formik";
+// import { useState } from "react";
+// import Button from "react-bootstrap/Button";
+// import Card from "react-bootstrap/Card";
+// import Modal from "react-bootstrap/Modal";
+
+// export default function CardItem({ note, deleteNote, getUserNotes }) {
+//   const [show, setShow] = useState(false);
+//   const handleClose = () => {
+//     setShow(false);
+//     setIsEditing(false);
+//   setNoteValues({});
+//   };
+//   const handleShow = () => setShow(true);
+//   const [isEditing, setIsEditing] = useState(false);
+//   const [noteValues, setNoteValues] = useState({});
+
+//   const initialValues = {
+//     title: isEditing ? noteValues.title : "",
+//     content: isEditing ? noteValues.content : "",
+//   };
+
+//   function updateNote(values) {
+//     axios
+//       .put(
+//         `https://note-sigma-black.vercel.app/api/v1/notes/${note._id}`,
+//         values,
+//         {
+//           headers: {
+//             token: `3b8ny__${localStorage.getItem("token")}`,
+//           },
+//         }
+//       )
+//       .then((res) => {
+//         console.log(res);
+//         getUserNotes();
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       })
+//       .finally(() => {
+//         handleClose();
+//       });
+//   }
+
+//   const { handleSubmit, handleChange, values } = useFormik({
+//     initialValues,
+//     enableReinitialize:true,
+//     onSubmit: updateNote,
+//   });
+
+//   function sendNotesValues(values) {
+//     console.log(values);
+//     setNoteValues(values);
+//     setIsEditing(true);
+//   }
+
+//   return (
+//     <div className="inner text-success p-4 rounded-3   position-relative">
+//       <Card className="card ">
+//         <Card.Body className="p-2">
+//           <Card.Title className=" text-capitalize fw-bold">
+//             {note.title}
+//           </Card.Title>
+//           <Card.Text>{note.createdAt.split("T", 1)}</Card.Text>
+//           <Card.Text>{note.content}</Card.Text>
+
+//           <Card.Footer className="card-footer text-muted p-3 d-flex align-items-center justify-content-between gap-2  ">
+//             <Button variant="danger" onClick={() => deleteNote(note._id)}> 
+//               Delete <i className="fa-solid fa-trash"></i>
+//             </Button>
+//             <Button
+//               variant="info"
+//               onClick={() => {
+//                 sendNotesValues(note);
+//                 handleShow();
+//               }}
+//             >
+//               edit <i className="fa-solid fa-pen-to-square"></i>
+//             </Button>
+//           </Card.Footer>
+//         </Card.Body>
+//       </Card>
+
+//       <Modal show={show} onHide={handleClose} backdrop="static">
+//         <Modal.Header closeButton>
+//           <Modal.Title>Edit Note</Modal.Title>
+//         </Modal.Header>
+//         <Modal.Body>
+//           <form action="">
+//             <input
+//               type="text"
+//               className="form-control my-4"
+//               placeholder="Note-Title"
+//               name="title"
+//               onChange={handleChange}
+//               value={values.title}
+//             />
+//             <textarea
+//               className="form-control"
+//               placeholder="Note Content"
+//               name="content"
+//               onChange={handleChange}
+//               value={values.content}
+//             ></textarea>
+//           </form>
+//         </Modal.Body>
+//         <Modal.Footer>
+//           <Button onClick={handleSubmit} variant="info">
+//             Update
+//           </Button>
+//         </Modal.Footer>
+//       </Modal>
+//     </div>
+//   );
+// }
+
+
+
+
+
 import axios from "axios";
 import { useFormik } from "formik";
 import { useState } from "react";
@@ -6,7 +128,7 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Modal from "react-bootstrap/Modal";
 
-export default function CardItem({ note, deleteNote, getUserNotes }) {
+export default function CardItem({ note, deleteNote, getUserNotes ,handleShowForDelete}) {
   const [show, setShow] = useState(false);
   const handleClose = () => {
     setShow(false);
@@ -16,6 +138,16 @@ export default function CardItem({ note, deleteNote, getUserNotes }) {
   const handleShow = () => setShow(true);
   const [isEditing, setIsEditing] = useState(false);
   const [noteValues, setNoteValues] = useState({});
+  const [deleteModal, setDeleteModal] = useState(false);
+
+
+  function handleShowForDelete() {
+    setDeleteModal(true);
+  }
+  function handleCloseForDelete() {
+    setDeleteModal(false);
+  }
+
 
   const initialValues = {
     title: isEditing ? noteValues.title : "",
@@ -51,6 +183,24 @@ export default function CardItem({ note, deleteNote, getUserNotes }) {
     onSubmit: updateNote,
   });
 
+
+    async function deleteNote(NoteId) {
+      const { data } = await axios.delete(
+        `https://note-sigma-black.vercel.app/api/v1/notes/${NoteId}`,
+        {
+          headers: {
+            token: `3b8ny__${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      console.log(data);
+  
+      getUserNotes(); //to display notes
+      toast.success("Note deleted");
+      handleCloseForDelete()
+  
+    }
+
   function sendNotesValues(values) {
     console.log(values);
     setNoteValues(values);
@@ -68,7 +218,7 @@ export default function CardItem({ note, deleteNote, getUserNotes }) {
           <Card.Text>{note.content}</Card.Text>
 
           <Card.Footer className="card-footer text-muted p-3 d-flex align-items-center justify-content-between gap-2  ">
-            <Button variant="danger" onClick={() => deleteNote(note._id)}>
+            <Button variant="danger" onClick={() => {handleShowForDelete()}}> 
               Delete <i className="fa-solid fa-trash"></i>
             </Button>
             <Button
@@ -113,6 +263,21 @@ export default function CardItem({ note, deleteNote, getUserNotes }) {
           </Button>
         </Modal.Footer>
       </Modal>
+
+
+      <Modal show={deleteModal} onHide={handleCloseForDelete}>
+        <Modal.Header closeButton>
+          <Modal.Title className="text-danger fw-bold">Deleting Note</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="text-danger">Are You Sure To Delete this Note ??</Modal.Body>
+        <Modal.Footer>
+          
+          <Button variant="primary" onClick={()=>{deleteNote(note._id)}}>
+            Confirm
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
     </div>
   );
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,55 +1,45 @@
 import axios from "axios";
 import { useFormik } from "formik";
 import React, { useContext, useEffect, useState } from "react";
-import { Button } from "react-bootstrap";
+import { Alert, Button } from "react-bootstrap";
 import Modal from "react-bootstrap/Modal";
 import toast from "react-hot-toast";
 import CardItem from "../../Components/Card/Card";
 import { counterContext } from "../../contexts/ConterContext";
-import { authContext } from "../../contexts/auth";
 
 export default function Home() {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   const [notes, setNotes] = useState([]);
- const {setCounter}= useContext(counterContext)
-
-
+  const { setCounter } = useContext(counterContext);
+ 
 
   const initialValues = {
-    title:  "",
-    content:"",
+    title: "",
+    content: "",
   };
 
-  function addNote(values) {
-    axios
-      .post("https://note-sigma-black.vercel.app/api/v1/notes", values, {
+  async function addNote(values) {
+    const { data } = await axios.post(
+      "https://note-sigma-black.vercel.app/api/v1/notes",
+      values,
+      {
         headers: {
           token: `3b8ny__${localStorage.getItem("token")}`,
         },
-      })
-      .then((res) => {
-        toast.success("Note has been Added");
-        getUserNotes() //to display notes => display the new notes every time we add a new one
-     
-      })
-      .catch((err) => {
-        console.log(err.response);
-        
-      })
-      .finally(() => {
-        handleClose();
-        //to close modal after added note dynamically
-      });
+      }
+    );
+    console.log(data);
+    handleClose();
+    toast.success("Note Has been added");
+    getUserNotes();
   }
 
   const { values, handleSubmit, handleChange } = useFormik({
     initialValues,
     onSubmit: addNote,
   });
-
-
 
   //Calling Api to get  user Notes
   async function getUserNotes() {
@@ -63,39 +53,39 @@ export default function Home() {
         }
       );
       console.log(data);
-      setNotes(data.notes.reverse());
-      setCounter(data.notes.length)
+      setNotes(data?.notes.reverse());
+      setCounter(data?.notes.length);
     } catch (error) {
-      setNotes(null)
-      setCounter(0) //to aviod the error during deleting the last note 
+      setNotes(null);
+      setCounter(0); //to aviod the error during deleting the last note
     }
- 
   }
-
 
   //calling to delete note
-  async function deleteNote(NoteId){
-    const {data} =await axios.delete(`https://note-sigma-black.vercel.app/api/v1/notes/${NoteId}`,{
-      headers:{
-        token: `3b8ny__${localStorage.getItem("token")}`
-      }
-    })
-    console.log( data)
-    getUserNotes() //to display notes
-    toast.success("Note deleted")
-    
-  }
+  // async function deleteNote(NoteId) {
+  //   const { data } = await axios.delete(
+  //     `https://note-sigma-black.vercel.app/api/v1/notes/${NoteId}`,
+  //     {
+  //       headers: {
+  //         token: `3b8ny__${localStorage.getItem("token")}`,
+  //       },
+  //     }
+  //   );
+  //   console.log(data);
+
+  //   getUserNotes(); //to display notes
+  //   toast.success("Note deleted");
+  //   handleCloseForDelete()
+
+  // }
 
   useEffect(() => {
-    getUserNotes(); //display notes 
+    getUserNotes(); //display notes
   }, []);
-
-
-
 
   return (
     <>
-       <div className="min-vh-100 p-2 home">
+      <div className="min-vh-100 p-2 home">
         <button
           className=" p-2 fs-2 text-info mt-2  btn-add fw-bolder d-block ms-auto rounded-2 border-0 "
           onClick={handleShow}
@@ -129,12 +119,32 @@ export default function Home() {
             </Button>
           </Modal.Footer>
         </Modal>
-      {notes ?  <div className="row g-2 mt-3">
-          {notes?.map((note) => {
-            return <div key={note._id} className="col-12 p-2" ><CardItem   note={note} deleteNote={deleteNote} getUserNotes={getUserNotes} /></div>
-          })}
-        </div> : <h1 className="text-danger my-4  fs-1 text-center text-capitalize">Hi <i className="fa-regular fa-hand-peace"></i> Your Note List is Empty !! </h1>}
-      </div>  
+        {notes ? (
+          <div className="row g-2 mt-3">
+            {notes?.map((note) => {
+              return (
+                <div key={note._id} className="col-12 p-2">
+                  <CardItem
+                    note={note}
+                    
+                    getUserNotes={getUserNotes}
+                   
+                  />
+                </div>
+              );
+            })}
+          </div>
+        ) : (
+          <h1 className="text-danger my-4  fs-1 text-center text-capitalize">
+            Hi <i className="fa-regular fa-hand-peace"></i> Your Note List is
+            Empty !!{" "}
+          </h1>
+        )}
+      </div>
+
+
+ 
+
     </>
   );
 }
